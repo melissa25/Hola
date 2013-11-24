6414c49ed37e5674806b910f695dcdb84e09ae9d
goldem64 <goldem64.master@aol.com>
2013-11-24 14:23:48 -0600

    a

--- chat_sockets_cliente_servidor/src/sockets/cliente_read.java
+++ chat_sockets_cliente_servidor/src/sockets/cliente_read.java
@@ -4,6 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 
 //archivo para leer desde el socket
+//otra cosa
 public class cliente_read implements Runnable {
 
 	public cliente_read() 
